3877e0893a01ba164bc9a2fb9b15a5c8937e1b3d
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-09-05 15:24:38 -0300

    V1.0

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>hola esto es un titulo</h1>
+      <p>
+        Hola esto es un parrafo de prueba para ver como funciona font source
+      </p>
+    </div>
+  );
+}
+
+export default App;
